c2bade649a8871d4d59f02f8c80fdc59db970150
HiteshVanol <148076015+HiteshVanol@users.noreply.github.com>
2025-12-27 16:12:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ const requireAuth = (req, res, next) => {
 };
 
 /* ---------------- FILE DOWNLOAD (IMPORTANT) ---------------- */
-app.get('/download/:file', (req, res) => {
+app.get('/files/:file', (req, res) => {
   const filePath = path.join(FILE_DIR, req.params.file);
 
   if (!fs.existsSync(filePath)) {
